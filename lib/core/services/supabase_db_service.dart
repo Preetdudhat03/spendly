4b61361d67539284ce342abf96c64f34504566b6
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-06-25 13:55:36 +0530

    25/6/2026

--- lib/core/services/supabase_db_service.dart
+++ lib/core/services/supabase_db_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:spendly/core/services/db_service.dart';
