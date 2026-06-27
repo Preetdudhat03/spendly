bb7e31865691118eb4a26f78bae16abe1fee48ea
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-06-27 19:00:26 +0530

    27/6/2026

--- lib/core/constants/config.dart
+++ lib/core/constants/config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   // 🔴 Set this to true to test the app incredibly fast in Offline Sandbox mode.
   // 🟢 Set this to false to connect to your real Supabase Database.
-  static const bool forceSandboxMode = false;
+  static const bool forceSandboxMode = true;
 
   static const String supabaseUrl = forceSandboxMode ? '' : 'https://rpugjnlabhvohnejynne.supabase.co'; 
   static const String supabaseAnonKey = forceSandboxMode ? '' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwdWdqbmxhYmh2b2huZWp5bm5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzNjE1OTgsImV4cCI6MjA5NzkzNzU5OH0.yCsKEg97_uGQvmyByga6AYA_OJpKquLMij49NGfdPPc'; 
