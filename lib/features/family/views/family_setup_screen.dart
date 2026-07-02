90dafe6fd88fe199a59fdbe2c0da119ec6e6c388
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-02 13:21:41 +0530

    2/7/2026

--- lib/features/family/views/family_setup_screen.dart
+++ lib/features/family/views/family_setup_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:spendly/core/providers/state_providers.dart';
 import 'package:spendly/core/widgets/shimmer_loading.dart';
-import 'package:spendly/core/widgets/shimmer_list_placeholder.dart';
 
 class FamilySetupScreen extends ConsumerStatefulWidget {
   const FamilySetupScreen({super.key});
