6c223e1c2a22a9fbc75fdc9423104da49845f3e7
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-09 23:53:00 +0530

    9/7/2026

--- lib/core/constants/config.dart
+++ lib/core/constants/config.dart
@@ -3,8 +3,13 @@ class AppConfig {
   // 🟢 Set this to false to connect to your real Supabase Database.
   static const bool forceSandboxMode = false;
 
-  static const String supabaseUrl = forceSandboxMode ? '' : 'https://rpugjnlabhvohnejynne.supabase.co'; 
-  static const String supabaseAnonKey = forceSandboxMode ? '' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwdWdqbmxhYmh2b2huZWp5bm5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzNjE1OTgsImV4cCI6MjA5NzkzNzU5OH0.yCsKEg97_uGQvmyByga6AYA_OJpKquLMij49NGfdPPc'; 
+  static const String supabaseUrl = forceSandboxMode 
+      ? '' 
+      : String.fromEnvironment('SUPABASE_URL', defaultValue: 'https://rpugjnlabhvohnejynne.supabase.co'); 
+      
+  static const String supabaseAnonKey = forceSandboxMode 
+      ? '' 
+      : String.fromEnvironment('SUPABASE_ANON_KEY', defaultValue: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwdWdqbmxhYmh2b2huZWp5bm5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzNjE1OTgsImV4cCI6MjA5NzkzNzU5OH0.yCsKEg97_uGQvmyByga6AYA_OJpKquLMij49NGfdPPc'); 
 
   static bool isSupabaseInitialized = false;
 
