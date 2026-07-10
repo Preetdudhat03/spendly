663454d4a484d4985ea6dea04c044a9241fe6b30
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-10 16:29:18 +0530

    10/7/2027

--- lib/features/analytics/presentation/pages/analytics_page.dart
+++ lib/features/analytics/presentation/pages/analytics_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:spendly/features/analytics/providers/analytics_providers.dart';
 import 'package:spendly/core/providers/state_providers.dart';
+import 'package:spendly/models/expense.dart';
 
 // Import all modular widgets
 import '../widgets/analytics_filter.dart';
