edbd8d261e0cbcdfcc1bb3dc9894da4868ca4f57
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-14 18:03:27 +0530

    14/7/2026

--- lib/features/expenses/views/all_expenses_screen.dart
+++ lib/features/expenses/views/all_expenses_screen.dart
@@ -237,8 +237,7 @@ class _AllExpensesScreenState extends ConsumerState<AllExpensesScreen> {
                         ],
                       ),
                     )
-                  : ListView.builder(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      padding: EdgeInsets.fromLTRB(16.0, 0, 16.0, isWide ? 0.0 : 100.0),
                       itemCount: groupedKeys.length,
                       itemBuilder: (context, groupIndex) {
                         final dateKey = groupedKeys[groupIndex];
