bfc137557bb34f5a62599f8d0f928c3c85a54a4a
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-17 01:21:07 +0530

    16/7/2026

--- lib/features/expenses/views/add_expense_screen.dart
+++ lib/features/expenses/views/add_expense_screen.dart
@@ -53,7 +53,7 @@ class _AddExpenseScreenState extends ConsumerState<AddExpenseScreen> {
     {'name': 'Electricity', 'iconPath': 'assets/icons/electricity.svg'},
     {'name': 'Medical', 'iconPath': 'assets/icons/medical.svg'},
     {'name': 'Insurances', 'iconPath': 'assets/icons/insurances.svg'},
-    {'name': 'Rent', 'emoji': '🏠', 'iconPath': 'assets/icons/rent.svg'},
+    {'name': 'Rent', 'iconPath': 'assets/icons/rent.svg'},
     {'name': 'Shopping', 'emoji': '🛍️', 'iconPath': 'assets/icons/shopping.svg'},
     {'name': 'Entertainment', 'emoji': '🎬', 'iconPath': 'assets/icons/entertainment.svg'},
     {'name': 'Education', 'emoji': '📚', 'iconPath': 'assets/icons/education.svg'},
