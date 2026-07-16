fa620efc243981c0871d6e337f32e4d633a79aca
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-17 00:03:12 +0530

    16/7/2026

--- lib/features/navigation/views/main_layout.dart
+++ lib/features/navigation/views/main_layout.dart
@@ -315,7 +315,7 @@ class _MainLayoutState extends ConsumerState<MainLayout> {
                   child: Icon(
                     isSelected ? selectedIcon : icon,
                     color: color,
-                    size: isAddButton ? 45 : 35,
+                    size: isAddButton ? 36 : 30,
                   ),
                 );
               },
