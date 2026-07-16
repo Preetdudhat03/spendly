447099751f2b029ad27fe9d703a930616910854a
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-17 01:19:44 +0530

    16/7/2026

--- lib/features/expenses/views/add_expense_screen.dart
+++ lib/features/expenses/views/add_expense_screen.dart
@@ -46,7 +46,7 @@ class _AddExpenseScreenState extends ConsumerState<AddExpenseScreen> {
     
     {'name': 'Food' , 'iconPath': 'assets/icons/food.svg'},
     {'name': 'Groceries', 'iconPath': 'assets/icons/groceries.svg'},
-    {'name': 'Petrol', 'emoji': '🚗', 'iconPath': 'assets/icons/petrol.svg'},
+    {'name': 'Petrol', 'iconPath': 'assets/icons/petrol.svg'},
     {'name': 'Recharges', 'emoji': '📱', 'iconPath': 'assets/icons/recharges.svg'},
     {'name': 'Travel', 'emoji': '✈️', 'iconPath': 'assets/icons/travel.svg'},
     {'name': 'Gas', 'emoji': '⛽', 'iconPath': 'assets/icons/gas.svg'},
