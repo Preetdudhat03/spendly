a824dc5f16b6b15062cf156cf2d7d435e022b60f
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-23 23:21:10 +0530

    chore: update lib/features/navigation/views/widgets/floating_spendly_navigation_bar.dart

--- lib/features/navigation/views/widgets/floating_spendly_navigation_bar.dart
+++ lib/features/navigation/views/widgets/floating_spendly_navigation_bar.dart
@@ -136,7 +136,7 @@ class FloatingSpendlyNavigationBar extends StatelessWidget {
                     style: TextStyle(
                       color: activeColor,
                       fontWeight: FontWeight.w700,
-                      fontSize: 13,
+                      fontSize: 13, //13
                       letterSpacing: -0.2,
                     ),
                   ),
