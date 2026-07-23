688332a46f2d54f084a8b32f240873bb08c56d0c
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-23 23:21:42 +0530

    chore: update lib/features/navigation/views/widgets/floating_spendly_navigation_bar.dart

--- lib/features/navigation/views/widgets/floating_spendly_navigation_bar.dart
+++ lib/features/navigation/views/widgets/floating_spendly_navigation_bar.dart
@@ -120,7 +120,7 @@ class FloatingSpendlyNavigationBar extends StatelessWidget {
             SvgPicture.asset(
               iconPath,
               width: 26, //22
-              height: 2, //22
+              height: 26, //22
               colorFilter: ColorFilter.mode(
                 isSelected ? activeColor : inactiveColor,
                 BlendMode.srcIn,
