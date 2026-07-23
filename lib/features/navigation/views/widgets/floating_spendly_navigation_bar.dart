da713c3a73c87182fbe69adbf1a9de711c3e013e
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-23 23:32:14 +0530

    chore: update lib/features/navigation/views/widgets/floating_spendly_navigation_bar.dart

--- lib/features/navigation/views/widgets/floating_spendly_navigation_bar.dart
+++ lib/features/navigation/views/widgets/floating_spendly_navigation_bar.dart
@@ -224,7 +224,7 @@ class __AddExpenseFloatingButtonState
       child: ScaleTransition(
         scale: _scaleAnimation,
         child: Container(
-          width: 52, //
+          width: 52, //52
           height: 52,
           decoration: BoxDecoration(
             shape: BoxShape.circle,
