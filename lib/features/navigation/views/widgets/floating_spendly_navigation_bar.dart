31e32134a8a9d3ce274af3c894f1739140db6436
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-23 23:31:35 +0530

    chore: update lib/features/navigation/views/widgets/floating_spendly_navigation_bar.dart

--- lib/features/navigation/views/widgets/floating_spendly_navigation_bar.dart
+++ lib/features/navigation/views/widgets/floating_spendly_navigation_bar.dart
@@ -233,7 +233,7 @@ class __AddExpenseFloatingButtonState
           child: Center(
             child: SvgPicture.asset(
               'assets/icons/plus.svg',
-              width: 30, //24
+              width: 3, //24
               height: 34, //24
               colorFilter: const ColorFilter.mode(
                 Colors.white,
