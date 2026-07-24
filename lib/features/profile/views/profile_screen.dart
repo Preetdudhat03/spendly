ffabebd1699a58ab19c1e74d7cec85e6136c53e8
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-24 10:57:51 +0530

    chore: update lib/features/profile/views/profile_screen.dart

--- lib/features/profile/views/profile_screen.dart
+++ lib/features/profile/views/profile_screen.dart
@@ -679,7 +679,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                         ),
                         const SizedBox(height: 4),
                         Text(
-                          'Developed by Preet Du',
+                          'Developed by Preet Dud',
                           style: TextStyle(
                             fontSize: 13,
                             color: Colors.grey[500],
