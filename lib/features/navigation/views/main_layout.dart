86482180ca87e38e52ec2926c329ebea8efb2da6
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-24 20:58:04 +0530

    chore: update lib/features/navigation/views/main_layout.dart

--- lib/features/navigation/views/main_layout.dart
+++ lib/features/navigation/views/main_layout.dart
@@ -45,7 +45,7 @@ class _MainLayoutState extends ConsumerState<MainLayout> {
       case 4:
         return 2; // Profile
       case 1:
-        return 3; // Add
+        return 2; // Add
       default:
         return 0;
     }
