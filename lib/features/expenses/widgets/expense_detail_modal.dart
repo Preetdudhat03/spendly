d28c8dc64dc7c34f40994ea1a34177ee852fb1b4
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-26 19:29:48 +0530

    chore: update lib/features/expenses/widgets/expense_detail_modal.dart

--- lib/features/expenses/widgets/expense_detail_modal.dart
+++ lib/features/expenses/widgets/expense_detail_modal.dart
@@ -554,12 +554,15 @@ class _EditExpenseFormState extends State<_EditExpenseForm> {
                     onPressed: _submit,
                     child: const Text('SAVE CHANGES'),
                   ),
+                  const SizedBox(height: 70),
                 ],
-                const SizedBox(height: 70),
+                
               ),
             ),
+            
           ),
         ),
+        
       ),
     );  
   }
