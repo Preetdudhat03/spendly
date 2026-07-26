2d8f086f6214082835117b5ebe3c0172b2051616
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-26 18:52:08 +0530

    chore: update lib/features/expenses/widgets/expense_detail_modal.dart

--- lib/features/expenses/widgets/expense_detail_modal.dart
+++ lib/features/expenses/widgets/expense_detail_modal.dart
@@ -230,7 +230,7 @@ void showExpenseDetail(BuildContext context, WidgetRef ref, Expense expense) {
                   ),
                 ],
               ),
-              const SizedBox(height: 80), // Padding to account for the floating navigation bar 80
+              const SizedBox(height: 75), // Padding to account for the floating navigation bar 80
             ],
           ),
           ),
