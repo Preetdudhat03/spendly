d07f27f77edc08ab3c5abcb242ed1815503cd57d
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-27 20:58:27 +0530

    chore: update lib/features/family/views/family_setup_screen.dart

--- lib/features/family/views/family_setup_screen.dart
+++ lib/features/family/views/family_setup_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:spendly/core/providers/state_providers.dart';
 import 'package:spendly/core/utils/schema_validator.dart';
 import 'package:spendly/core/widgets/shimmer_loading.dart';
