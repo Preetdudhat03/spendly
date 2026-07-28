ad6eeb56368c004a7a101f27b8d3b9fe15bc6a8a
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-07-28 15:42:42 +0530

    chore: update lib/features/profile/views/profile_screen.dart

--- lib/features/profile/views/profile_screen.dart
+++ lib/features/profile/views/profile_screen.dart
@@ -160,7 +160,7 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
     showModalBottomSheet(
       context: context,
       builder: (context) => Padding(
-        padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 100.0),
+        padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 105.0),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
