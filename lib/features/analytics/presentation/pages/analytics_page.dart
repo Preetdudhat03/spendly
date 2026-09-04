a81b96f981a4989cd8053e46fce87e97d50ee285
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-09-04 16:21:34 +0530

    chore: update lib/features/analytics/presentation/pages/analytics_page.dart

--- lib/features/analytics/presentation/pages/analytics_page.dart
+++ lib/features/analytics/presentation/pages/analytics_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:spendly/features/analytics/providers/analytics_providers.dart';
 import 'package:spendly/core/providers/state_providers.dart';
+import 'package:spendly/core/widgets/capsule_top_bar.dart';
 import 'package:spendly/models/expense.dart';
 
 // Import all modular widgets
