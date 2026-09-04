1e4c97829e52bfbaddaa96f8a8a0e8676c1a8e23
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-09-04 17:40:34 +0530

    chore: update android/app/build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,22 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+} else {
+    val appKeystorePropertiesFile = file("key.properties")
+    if (appKeystorePropertiesFile.exists()) {
+        keystoreProperties.load(FileInputStream(appKeystorePropertiesFile))
+    }
 }
 
 android {
@@ -20,21 +34,45 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.family.spendly.spendly"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            val keyAliasProp = keystoreProperties.getProperty("keyAlias") ?: System.getenv("KEY_ALIAS") ?: "spendly"
+            val keyPasswordProp = keystoreProperties.getProperty("keyPassword") ?: System.getenv("KEY_PASSWORD") ?: "spendlyapp2026"
+            val storePasswordProp = keystoreProperties.getProperty("storePassword") ?: System.getenv("STORE_PASSWORD") ?: "spendlyapp2026"
+            val storeFileProp = keystoreProperties.getProperty("storeFile") ?: "spendly-release.jks"
+
+            keyAlias = keyAliasProp
+            keyPassword = keyPasswordProp
+            storePassword = storePasswordProp
+
+            val customStoreFile = file(storeFileProp)
+            val parentStoreFile = rootProject.file(storeFileProp)
+            val appStoreFile = file("spendly-release.jks")
+
+            storeFile = when {
+                customStoreFile.exists() -> customStoreFile
+                parentStoreFile.exists() -> parentStoreFile
+                appStoreFile.exists() -> appStoreFile
+                else -> null
+            }
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            val releaseSigningConfig = signingConfigs.getByName("release")
+            signingConfig = if (releaseSigningConfig.storeFile != null && releaseSigningConfig.storeFile!!.exists()) {
+                releaseSigningConfig
+            } else {
+                signingConfigs.getByName("debug")
+            }
         }
     }
 }
