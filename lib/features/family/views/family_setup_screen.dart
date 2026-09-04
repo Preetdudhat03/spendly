5a622bedff01bb9c29e92bc41030e8e2acf6bc4d
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-09-04 16:22:17 +0530

    chore: update lib/features/family/views/family_setup_screen.dart

--- lib/features/family/views/family_setup_screen.dart
+++ lib/features/family/views/family_setup_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:spendly/core/providers/state_providers.dart';
 import 'package:spendly/core/utils/schema_validator.dart';
+import 'package:spendly/core/widgets/capsule_top_bar.dart';
 import 'package:spendly/core/widgets/shimmer_loading.dart';
 
 class FamilySetupScreen extends ConsumerStatefulWidget {
