00d185f5c1d369dc77cc2ec7b439ba72fdd3ddd8
Preet Dudhat <preet.dudhat0307@gmail.com>
2026-09-09 21:25:04 +0530

    chore: update lib/features/expenses/widgets/expense_detail_modal.dart

--- lib/features/expenses/widgets/expense_detail_modal.dart
+++ lib/features/expenses/widgets/expense_detail_modal.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:spendly/core/providers/state_providers.dart';
 import 'package:spendly/core/utils/schema_validator.dart';
+import 'package:spendly/core/utils/currency_formatter.dart';
 import 'package:spendly/models/expense.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
